93eec7c4e1aff0699c28aeb6cc7e756af2703dfd
RIAcde <riauz24032003@gmail.com>
2024-10-13 14:53:56 +0500

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import CardItem from './components/CardItem'
 import Cards from './components/Cards'
+import Contact from './components/Contact'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 import Cart from './pages/cart'
 import CheckOut from './pages/checkout'
 import Homepage from './pages/homepage'
 import Likes from './pages/likes'
-import Register from './pages/register'
-import Contact from './components/Contact'
 import NotFound from './pages/NotFound'
+import Register from './pages/register'
 function App() {
 	return (
 		<div className='relative'>
@@ -20,7 +20,10 @@ function App() {
 				<Route path='/checkout' element={<CheckOut />} />
 				<Route path='/register' element={<Register />} />
 				<Route path='/cart' element={<Cart />} />
-
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/cards' element={<Cards />} />
+				<Route path='/carditem' element={<CardItem />} />
+				<Route path='*' element={<NotFound />} />
 			</Routes>
 			<Footer />
 		</div>
